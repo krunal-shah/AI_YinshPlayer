3eeffed577751ea0f389a4fb9b00023bbf5d54a9
Krunal Shah <ktgshah@gmail.com>
2018-11-05 22:36:41 +0530

    Added folder in simlation

--- simulate/simulate.sh
+++ simulate/simulate.sh
@@ -1,10 +1,11 @@
 g++ -std=c++11 -Ofast -march=native -o simulate ../src/Board.cpp ../src/Marker.cpp ../src/Ring.cpp ../src/Solver.cpp ../src/simulate.cpp ../src/Util.cpp
 x1=$(expr $1)
+mkdir $2
 for value1 in `seq 1 $x1`
 do
 	for value2 in `seq 1 $x1`
 	do
 		echo $value2
-		./simulate $value1 $value2 > $value1'_'$value2'.txt'
+		./simulate $value1 $value2 > './'$2'/'$value1'_'$value2'.txt'
 	done
 done
